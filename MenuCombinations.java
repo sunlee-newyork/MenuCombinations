8be42b0310a2dbefda1ac1cf476b04e5d8954865
Sun Lee <hi@sunlee.io>
2019-01-28 08:53:39 -0500

    Remove console logs, add instructions

--- MenuCombinations.java
+++ MenuCombinations.java
@@ -3,37 +3,23 @@ import java.util.*;
 
 class MenuCombinations {
 	public static void menu(double menu[], double budget, ArrayList ret, int i) {
-		System.out.println("");
-		System.out.println("===> menu() called.");
-        
-        if (budget == 0) {
-        	System.out.print("====== DONE ====== Final set: ");
-            System.out.println(ret);
+		if (budget <= 0) {
+			if (budget == 0) {
+				System.out.println(ret);
+			}
 
-            return;
-        }
+			return;
+		}
 
-        if (budget <= 0) {
-        	System.out.println("NO MATCH. RETURNING.");
-            return;
-        }
+		if (i == menu.length) {
+			return;
+		}
 
-        if (i == menu.length) {
-        	System.out.println("MENU LENGTH DONE");
-            return;
-        }
+		ret.add(menu[i]);
+		menu(menu, budget - menu[i], ret, i + 1);
 
-        System.out.print("Adding menu item to ret: ");
-        System.out.print(menu[i]);
-        System.out.println("");
-        ret.add(menu[i]);
-        menu(menu, budget - menu[i], ret, i + 1);
-
-        ret.remove(ret.size() - 1);
-        System.out.print("Removing ret item: ");
-        System.out.print(ret);
-        System.out.println("");
-        menu(menu, budget, ret, i + 1);
+		ret.remove(ret.size() - 1);
+		menu(menu, budget, ret, i + 1);
 	}
 
 	public static void main(String[] args) {
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MenuCombinations
 Find all combinations of a menu given a budget.
+
+## Instructions
+
+* Install Java and JRE
+* Run `javac MenuCombinations.java`
+* Run `java MenuCombinations`
